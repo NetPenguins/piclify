8ffbbf0a0b464e0cb98f8ee230b329174511ca40
NetPenguins <cwils192@uncc.edu>
2020-10-05 21:54:57 -0400

    Move Style into layout component.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,33 +1,35 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import "bulma/css/bulma.css"
+//import "bulma/css/bulma.css"
 import "../styles/main.css"
 import Navbar from "./Navbar"
 import Footer from "./footer"
+import styled from "styled-components"
+
+
+const MainContent = styled.div`
+    min-height: 75vmax;
+    width: auto;
+    overflow-x: hidden;
+    background: linear-gradient(90deg, #000000 -3.07%, #3d3d3d 100%);
+    color: rgb(32, 32, 32);
+    padding-bottom: 5vh;
+`
+const Section = styled.div`
+    flex: 1;
+`
 
 const Layout = ({ children }) => {
-  //left in so you can use Title in your Layout 
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
     return (
-      <main className="MainContent">
+      <MainContent>
         <Navbar/>
-        <div
-          className="MainContent"
-        >
-          <main >{children}</main>
-        </div>
-        <div className="section"/>
+        <MainContent>
+          <main>{children}</main>
+        </MainContent>
+        <Section/>
         <Footer/>
-      </main>
+      </MainContent>
   )
 }
 Layout.propTypes = {
